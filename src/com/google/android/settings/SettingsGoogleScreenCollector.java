474a073f3877589167ecebfa5f584464affa9098
ralph950412 <ralph950412@gmail.com>
2025-07-16 03:17:00 +0800

    Settings: Implement reverse-engineered battery saver page
    This change introduces the battery saver page from Pixel along with:
    
    - Schedules and reminders
    - Extreme Battery Saver
    - Adaptive Battery
    
    Co-authored-by: Lunia <lunia0251@proton.me>
    
    Change-Id: Ieec7a777e7ae04d9d080060b06c024ba8aa824cf

--- src/com/google/android/settings/SettingsGoogleScreenCollector.java
+++ src/com/google/android/settings/SettingsGoogleScreenCollector.java
@@ -3,16 +3,28 @@ package com.google.android.settings;
 import com.android.settingslib.metadata.FixedArrayMap;
 import com.android.settingslib.metadata.PreferenceScreenMetadataFactory;
 
+import com.google.android.settings.fuelgauge.batterysaver.AdaptiveBatteryScreen;
+import com.google.android.settings.fuelgauge.batterysaver.BatterySaverGoogleScreen;
+import com.google.android.settings.fuelgauge.batterysaver.BatterySaverScheduleScreen;
 import com.google.android.settings.update.SoftwareUpdateScreen;
 
 public abstract class SettingsGoogleScreenCollector {
 
     public static FixedArrayMap<String, PreferenceScreenMetadataFactory> get() {
-        return new FixedArrayMap<>(1, SettingsGoogleScreenCollector::init);
+        return new FixedArrayMap<>(4, SettingsGoogleScreenCollector::init);
     }
 
     private static void init(
             FixedArrayMap.OrderedInitializer<String, PreferenceScreenMetadataFactory> initializer) {
+        initializer.put(
+                "adaptive_battery_entry",
+                (PreferenceScreenMetadataFactory) AdaptiveBatteryScreen::new);
+        initializer.put(
+                "battery_saver_schedule",
+                (PreferenceScreenMetadataFactory) context -> new BatterySaverScheduleScreen());
+        initializer.put(
+                "battery_saver_screen",
+                (PreferenceScreenMetadataFactory) context -> new BatterySaverGoogleScreen());
         initializer.put(
                 "software_update_settings_v2",
                 (PreferenceScreenMetadataFactory) context -> new SoftwareUpdateScreen());
